c6b7063e884e456c6dacadfb378a4c7000d8654d
root <root@ip-172-42-0-162.ap-south-1.compute.internal>
2018-09-11 10:18:22 +0000

    testing for auto build2

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
     class Helloworld{  
         public static void main(String args[]){  
-         System.out.println("Hello Java newwwwwwwppppppppw!!!!!");  
+         System.out.println("Hellooooooo Java newwwwwwwppppppppw!!!!!");  
         }  
     }  
